8a60e4d094022c8b718966f3e3da46f0941b2b88
Anastasia <ans.dudarenko@gmail.com>
2018-08-26 12:36:01 +0300

    Initial commit

---
+++ hello_world_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    my_dict = {'insert_me': 'Values'}
+    return render(request, 'hello_world_app/index.html', context=my_dict)
+
+# Create your views here.
